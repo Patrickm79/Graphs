d73adc6fd0f2f583927726b4a0a43168d5863101
Patrick Millet <lohs79seahawk@gmail.com>
2020-10-08 18:12:57 -0500

    adv WIP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -3,11 +3,11 @@ from player import Player
 from world import World
 
 import random
-from ast import literal_eval
+import collections
+from ast import Str, literal_eval
 
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -24,12 +24,98 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+player.current_room = world.starting_room
+
+# Helpers
+
+def add_relationship(starting_room, direction, ending_room):
+    room_map[starting_room][direction] = ending_room
+    room_map[ending_room][inverse[direction]] = starting_room
+
+def add_room(room):
+    if room not in room_map:
+        room_map[room] = {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+
+        exits = room.get_exits()
+
+        for direction in room_map[room]:
+            if direction not in exits:
+                room_map[room][direction] = 'NONE'
+
+        return True
+    else:
+        return False
+
+def closest_room_with_unexplored_exits(starting_room):
+    visited = set()
+    path_queue = collections.deque()
+
+    path_queue.append((starting_room, []))
+
+    while len(path_queue) > 0:
+        room, path = path_queue.popleft()
+
+        if room in visited:
+            continue
+        else:
+            visited.add(room)
+
+        for exit_direction in room_map[room]:
+            if room_map[room][exit_direction] == '?':
+                return path
+            elif room_map[room][exit_direction] != 'NONE':
+                new_path = path.copy()
+                new_path.append(exit_direction)
+                new_room = room_map[room][exit_direction]
+                path_queue.append((new_room, new_path))
+
+    return []
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+prevailing_direction = 's'
 
+change_direction = {'n': 'e', 'e': 's', 's': 'w', 'w': 'n'}
+inverse = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+room_map = {}
+
+add_room(player.current_room)
+
+while len(room_map) < len(room_graph):
+    room = player.current_room
+    exits = room.get_exits()
+
+    found_unexplored_exit = False
+    direction = prevailing_direction
+
+    for _ in range(len(change_direction)):
+        if room_map[room][direction] == '?' and direction in exits:
+            found_unexplored_exit = True
+
+
+            player.travel(direction)
+            next_room = player.current_room
+            traversal_path.append(direction)
+            # If already visisted, backtrack and take off traversal path
+            if not add_room(next_room):
+                player.travel(inverse[direction])
+                traversal_path.pop()
+
+            add_relationship(room, direction, next_room)
+            break
+        else:
+            direction = change_direction[direction]
+
+
+    if not found_unexplored_exit:
+        path = closest_room_with_unexplored_exits(room)
+
+        traversal_path.extend(path)
+        for direction in path:
+            player.travel(direction)
 
 # TRAVERSAL TEST
 visited_rooms = set()
